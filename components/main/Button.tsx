86f65d7d238b252f31a16f2818e901bd9888ee69
goodgains-dev <159225098+goodgains-dev@users.noreply.github.com>
2024-08-04 12:10:51 -0400

    Update Button.tsx
    1

--- components/main/Button.tsx
+++ components/main/Button.tsx
@@ -1,16 +1,41 @@
-import React from "react";
+"use client";
 
-const Button = ({ className, href, onClick, px, white, children }) => {
+import React from "react";
+import PropTypes from "prop-types";
+
+/**
+ * @typedef {Object} ButtonProps
+ * @property {string} [className]
+ * @property {string} [href]
+ * @property {function} [onClick]
+ * @property {number} [px]
+ * @property {boolean} [white]
+ * @property {React.ReactNode} [children]
+ */
+
+/**
+ * @param {ButtonProps} props
+ */
+const Button = ({ className = '', href = '', onClick = () => {}, px = 16, white = false, children }) => {
   return (
     <a
       href={href}
       onClick={onClick}
       className={`py-2 px-4 border-2 rounded-lg text-center text-white bg-gradient-to-r from-orange-500 to-orange-500 hover:from-orange-400 hover:via-red-600 hover:to-orange-400 ${className}`}
-      style={{ padding: `${px}px` }}
+      style={{ paddingLeft: `${px}px`, paddingRight: `${px}px` }}
     >
       {children}
     </a>
   );
+};
+
+Button.propTypes = {
+  className: PropTypes.string,
+  href: PropTypes.string,
+  onClick: PropTypes.func,
+  px: PropTypes.number,
+  white: PropTypes.bool,
+  children: PropTypes.node.isRequired,
 };
 
 export default Button;
