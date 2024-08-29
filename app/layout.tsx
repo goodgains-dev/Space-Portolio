5b6900f9ff2d73ba0fed67b8b6860d033d8f9b3f
goodgains-dev <159225098+goodgains-dev@users.noreply.github.com>
2024-08-29 13:34:06 -0400

    Update layout.tsx
    1

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,8 @@ import "./globals.css";
 import StarsWithParticles from "@/components/main/StarsWithParticles";
 import Navbar from "@/components/main/Navbar";
 import Footer from "@/components/main/Footer";
-import Sidebar from "@/components/main/Sidebar"; // Import Sidebar from the correct file path
+import Sidebar from "@/components/main/Sidebar";
+import MobileRedirect from "@/components/MobileRedirect";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -23,8 +24,9 @@ export default function RootLayout({
       <body
         className={`${inter.className} bg-[#030014] overflow-y-scroll overflow-x-hidden`}
       >
+        <MobileRedirect />
         <StarsWithParticles />
-        <Sidebar /> {/* Add Sidebar component here */}
+        <Sidebar />
         <Navbar />
         {children}
         <Footer />
